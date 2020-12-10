c759267308274cd48f01aeb0f54bc221bdbb3d22
rich wilk <richardwilkinson@live.com>
2020-12-09 20:25:20 -0500

    added constructor for engineer and two tests that all pass

---
+++ test/engineer.test.js
@@ -0,0 +1,14 @@
+const engineer = require("../lib/Engineer");
+const { TestScheduler } = require("jest");
+const Engineer = require("../lib/Engineer");
+
+test("sets github account via constructor arguments", () => {
+    const testValue = "GitHubMan";
+    const e = new Engineer("POO", 100, "test@testez.com", testValue);
+    expect(e.github).toBe(testValue);
+})
+test("gets github username via getGithub()", () => {
+    const testValue = "githubuser";
+    const e = new Engineer("POO", 100, "test@testez.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+})
